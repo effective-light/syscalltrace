5db7ace794a0fe4aeffa8f0d436631b309379fc4
effective-light <someguy@effective-light.com>
2021-01-07 21:04:06 -0500

    Implement basic system call tracing

--- ctrace.c
+++ ctrace.c
@@ -3,19 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <sys/ptrace.h>
-
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+
+#include <sys/ptrace.h>
+
+#define handle_error(msg) { perror(msg); exit(EXIT_FAILURE); }
 
 
 static long safe_ptrace(enum __ptrace_request request, pid_t pid,
         void *addr, void *data) {
     long ret;
     if ((ret = ptrace(request, pid, addr, data)) == -1) {
-        perror("ptrace");
-        exit(EXIT_FAILURE);
+        handle_error("ptrace");
     }
 
     return ret;
@@ -30,27 +31,42 @@ int main(int argc, char **argv) {
     pid_t pid = fork();
     if (!pid) {
         safe_ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-        raise(SIGSTOP);
         execvp(argv[1], (argv + 1));
         perror("exec");
     } else if (pid > 0) {
-        safe_ptrace(PTRACE_ATTACH, pid, NULL, NULL);
         printf("pid: %d\n", pid);
         int status;
+        struct __ptrace_syscall_info info;
+        size_t size = sizeof(info);
+        if (waitpid(pid, &status, 0) == -1) {
+            handle_error("waitpid");
+        }
+        safe_ptrace(PTRACE_SETOPTIONS, pid, NULL,
+                (void *) PTRACE_O_TRACESYSGOOD);
+        safe_ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
         do {
-            if (waitpid(pid, &status, WCONTINUED) == -1) {
-                perror("waitpid");
-                return EXIT_FAILURE;
+            if (waitpid(pid, &status, 0) == -1) {
+                handle_error("waitpid");
             }
             if (WIFSTOPPED(status)) {
-                // TODO: read syscall
-                if (kill(pid, SIGCONT) == -1) {
-                    perror("kill");
-                    return EXIT_FAILURE;
+                safe_ptrace(PTRACE_GET_SYSCALL_INFO, pid, (void *) size, &info);
+                switch (info.op) {
+                    case PTRACE_SYSCALL_INFO_ENTRY:
+                        printf("%d()", info.entry.nr);
+                        break;
+                    case PTRACE_SYSCALL_INFO_EXIT:
+                        printf(" = %ld\n", info.exit.rval);
+                        break;
+                    case PTRACE_SYSCALL_INFO_SECCOMP:
+                        break;
+                    default:
+                        break;
                 }
+
+                safe_ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
             }
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        printf("tracer exit\n");
+        printf("\ntracer exit\n");
         return 0;
     } else {
         perror("fork");
